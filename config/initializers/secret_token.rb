467ee7073c4cf0f909915a3333ce62622761ac67
root <root@cd20e0e27e6f.(none)>
2014-03-06 23:59:29 +0000

    New Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DockerDemo::Application.config.secret_key_base = '207cf1b7c483a62731c74de86c552a5861edbbdfa3a4f02f2c974557766c6cb9ecc5f110ad65416e694190dafff07d0fc7ae3cf3ed809cae535dd7bbcd5e145d'
